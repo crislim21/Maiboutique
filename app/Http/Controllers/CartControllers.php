0407944fb6574d2f1294fce0e92b8cb541728361
crislim21 <reavleaz@yahoo.co.id>
2022-11-24 18:28:18 +0700

    Success

--- app/Http/Controllers/CartControllers.php
+++ app/Http/Controllers/CartControllers.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Models\Cart;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 class CartControllers extends Controller
 {
@@ -91,9 +93,46 @@ class CartControllers extends Controller
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, $index)
     {
-        //
+        // $user = auth()->user()->id;
+        // dd($index);
+        // $data = Cart::where($index, auth()->user()->id)->get();
+        $item = DB::table('carts')->select('*')
+            ->where('id', '=', $index)->get();
+        $coba = $item[0]->post_id;
+        $product = Post::findOrFail($item[0]->post_id);
+        // $data = DB::table('carts')->select('*')->where('carts.id', $user)->get();
+        // dd($product, $item[0]);
+        // dd($index);
+
+        $dummy = DB::table('users')
+                    ->join('carts', 'users.id', '=', 'carts.user_id')
+                    ->where('carts.user_id', '=', Auth::user()->id)
+                    ->select('carts.*')->get();
+        $count = $dummy->count();
+        // dd($dummy[2]->id);
+        // dd($dummy);
+        $item = Post::find($index);
+        for($i=0;$i<$count;$i++) {
+
+            $idx = $dummy[$i]->id;
+            if($idx == $item->id){
+                break;
+            }
+        }
+        // dd($idx);
+        // dd($dummy,$item->id);
+        $validatedData = $request->validate([
+            'quantity' => ['required', 'min:1']
+        ]);
+        // dd($validatedData['quantity']);
+        // $update = DB::table('carts')->where('post')
+
+        Cart::where('id', $idx)
+            ->update(['quantity' => $validatedData['quantity']]);
+        return redirect('/cart')->with('success','Product Quantity diubah');
+
     }
 
     /**
